1eee3e09452604efebb4cccdc858902a8a06d2aa
Rachel <rhsia1014@gmail.com>
2020-09-12 17:03:00 +0800

    all

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,8 +5,12 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("openslam_gmapping")
 subdirs("serial")
+subdirs("slam_gmapping/slam_gmapping")
 subdirs("ros-keyboard")
 subdirs("new_serial")
 subdirs("test_topic")
 subdirs("rplidar_ros")
+subdirs("slam_gmapping/gmapping")
+subdirs("odom")
